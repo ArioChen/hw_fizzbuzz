7431dfeeb5f0860e3d9ad12f501af358a12929b4
ArioChen <ario03271028@yahoo.com.tw>
2017-09-21 09:37:52 +0800

    modify fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
 ## write your fizzbuzz method in this file
 # see http://en.wikipedia.org/wiki/Fizz_buzz for details on FizzBuzz game
+def fizzbuzz(number)
+	fizz_array = Array(1..number)
+	fizz_array.each do |i|
+		fizz_array[i-1] ="Fizz" if i%3==0 
+		fizz_array[i-1] ="Buzz" if i%5==0
+		fizz_array[i-1] ="FizzBuzz" if (i%3==0 && i%5 == 0)
+	end	
+  return fizz_array
+end
